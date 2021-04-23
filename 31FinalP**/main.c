75ca6b75859a5f57badd8a95647322d03864093b
CharlieMoll <charliemoll@email.arizona.edu>
2021-04-23 13:21:03 -0700

    Initial Commit

---
+++ 31FinalP**/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  31FinalP**
+//
+//  Created by Charles Moll on 4/23/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
