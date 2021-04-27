83c91766d9a69ea39c521ad5fa592296be57866f
EdwardVanica <83107982+EdwardVanica@users.noreply.github.com>
2021-04-26 18:41:29 -0700

    Update main.c

--- 31FinalP**/main.c
+++ 31FinalP**/main.c
@@ -11,6 +11,21 @@ int main(void){
 
     printf("hello World");
     
+    // ask the user should load from file ? or just shuffle a deck ?
+	printf("Do you want to load the cards from the file ? (y/n)\n");
+	scanf("%c", userResponse);
+	if (userResponse == 'y')
+	{
+		ReadInCards(&stockPile);
+	}
+	else
+	{
+		CreateDeck(&stockPile);
+		ShuffleCards(&stockPile);
+		ShuffleCards(&stockPile);
+	}
+	
+    
     
     return 0;
 }
